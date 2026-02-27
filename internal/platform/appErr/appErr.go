d56e68cef4f538875907e5e9785b4f9202474aa7
Guido do Rego <guidodorego@gmail.com>
2026-02-27 10:07:37 -0300

    correct message of MethodNotAllowed in appErr

--- internal/platform/appErr/appErr.go
+++ internal/platform/appErr/appErr.go
@@ -5,6 +5,6 @@ import "errors"
 var (
 	NotFound         = errors.New("user not found")
 	Unauthorized     = errors.New("Unauthorized")
-	MethodNotAllowed = errors.New("Unauthorized")
+	MethodNotAllowed = errors.New("Method not allowed")
 	Internal         = errors.New("Internal Error")
 )
